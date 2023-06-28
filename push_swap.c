f7acb297f1ee52b91b2a5c25eb1d8232a734aa22
Victor Terroso <v.terroso87@gmail.com>
2023-06-28 13:16:43 +0200

    movements

--- push_swap.c
+++ push_swap.c
@@ -5,14 +5,15 @@ int main(int ac, char **av)
     t_stack *stack_a;
     t_stack *stack_b;
 
-    stack_a = NULL;
-    stack_b = NULL;   
     if (ac < 2)
         return (0);
     ft_checkargs(ac, av);
-
+    stack_a = NULL;
+    stack_b = NULL;
+    ft_add_stack(&stack_a, ac, av);   
+    if (is_sorted(stack_a))
+        return (0);
+    ft_sort(&stack_a, &stack_b);
     
-
-
     return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -14,5 +14,24 @@ typedef struct s_stack
 void    ft_error(void);
 void ft_check_args(int ac, char **av);
 int is_sorted(t_stack *stack);
+void ft_add_stack(t_stack **stack, int ac, char **av);
+void ft_free(t_stack **stack);
+void ft_sort(t_stack **stack_a, t_stack **stack_b);
+int ft_stacklen(t_stack *stack);
+int ft_min(int *arr, int len);
+int ft_max(int *arr, int len);
+void ft_sa(t_stack **stack);
+void ft_sb(t_stack **stack);
+void ft_ss(t_stack **stack_a, t_stack **stack_b);
+void ft_pa(t_stack **stack_a, t_stack **stack_b);
+void ft_pb(t_stack **stack_a, t_stack **stack_b);
+void ft_ra(t_stack **stack);
+void ft_rb(t_stack **stack);
+void ft_rr(t_stack **stack_a, t_stack **stack_b);
+void ft_rra(t_stack **stack);
+void ft_rrb(t_stack **stack);
+void ft_rrr(t_stack **stack_a, t_stack **stack_b);
+void ft_print_stack(t_stack *stack);
+
 
 #endif
